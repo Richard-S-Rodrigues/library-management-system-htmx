de355fa57616d3dca7fa496ff81f321f1e0f9759
Richard-S-Rodrigues <richardsouzarodrigues555@gmail.com>
2023-11-02 16:48:57 -0300

    Adjust BookEdit page to save book

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,96 +5,73 @@ using LibraryManagementSystemHtmx.Services.BookService;
 
 namespace LibraryManagementSystemHtmx.Pages;
 
+[IgnoreAntiforgeryToken]
 public class IndexModel : PageModel
 {
-    IEnumerable<Book> mockBooks = new List<Book>() 
-    {
-        new() 
-        {
-            Id = 1,
-            Title = "Test title 1",
-            Description = "Test description 1"
-        },
-        new() 
-        {
-            Id = 2,
-            Title = "Test title 2",
-            Description = "Test description 2"
-        }
-    };
     private readonly IBookService _bookService;
-    private IEnumerable<Book> _books;
-    public IEnumerable<Book> books
-    {
-        get 
-        {
-            if (_books == null)
-            {
-                _books =  mockBooks;
-            }
-            return _books;
-        }
-        private set 
-        {
-            _books = value;
-        }
-    } 
+    public IList<Book> books { get; set; } = new List<Book>(); 
 
-    private TableData _bookTable;
-    public TableData bookTable 
-    {
-        get
-        {
-            if (_bookTable == null)
-            {
-                _bookTable = new TableData("Title", "Description");
+    public TableData bookTable { get; set; } = new();
 
-                foreach (var book in books)
-                {
-                    _bookTable.AddRow(book.Id!.Value, book);
-                }
-            }
-            return _bookTable;
-        }
-        private set 
-        {
-            _bookTable = value;
-        }
-    }
+    [BindProperty]
+    public Book currentSelectedBook { get; set; } = new();
 
     public IndexModel(IBookService bookService)
     {
         _bookService = bookService;
     }
 
-    public void SaveBook()
+    public async Task<IActionResult> OnPostSaveBook()
     {
+        if (ModelState.IsValid)
+        {
+            if (currentSelectedBook.Id is null) 
+            { 
+                currentSelectedBook = await _bookService.Create(currentSelectedBook);
+            }
+            else 
+            {
+                await _bookService.Update(currentSelectedBook.Id.Value, currentSelectedBook);
+            }
+            
+            return RedirectToPage();
+        }
+
+        return Page();
+    }
+
+    public async Task OnGetAsync()
+    {
+        bookTable = new TableData("Isbn", "Title", "Author", "Description");
+        books = await _bookService.GetAll();
         
+        foreach (var book in books)
+        {
+            bookTable.AddRow(book.Id!.Value, book);
+        }
     }
 
-    public async void OnGet()
-    {
-        _books = await _bookService.GetAll();
-    }
-
-    public IActionResult OnGetOpenEditModal(int? rowId)
+    public async Task<IActionResult> OnGetOpenEditModal(int? id)
     {    
-        if (rowId is null)
+        if (id is null)
         {
-            return Partial("~/Pages/BookEdit.cshtml", new Book());
+            return Partial("~/Pages/BookEdit.cshtml", this);
         }
         
-        var row = bookTable.GetRowById(rowId.Value);
-        if (row is null)
+        currentSelectedBook = await _bookService.Get(id.Value);
+        
+        if (currentSelectedBook is null)
         {
-            return Partial("~/Pages/BookEdit.cshtml", new Book());
+            return Partial("~/Pages/BookEdit.cshtml", this);
         }
-        return Partial("~/Pages/BookEdit.cshtml", (Book)row.Value);
+
+        return Partial("~/Pages/BookEdit.cshtml", this);
     }
 
-    public void OnPostDeleteAction(int rowId)
+    public async Task OnPostDeleteAction(int rowId)
     {
-        bookTable.RemoveRow(rowId);
+        await _bookService.Delete(rowId);
+        await OnGetAsync();
     }
 
     public IActionResult OnGetSearch(string searchQuery)
@@ -105,7 +82,7 @@ public class IndexModel : PageModel
             : rows.Where(
                 row => 
                     row.Title.Contains(searchQuery, StringComparison.OrdinalIgnoreCase) || 
-                    row.Description.Contains(searchQuery, StringComparison.OrdinalIgnoreCase)
+                    row.Description!.Contains(searchQuery, StringComparison.OrdinalIgnoreCase)
             ).ToList();
 
         TableData updatedTable = bookTable;
