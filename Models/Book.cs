ccd2c815fc00167a0cdd5204a5e3b74edc236dae
Richard-S-Rodrigues <richardsouzarodrigues555@gmail.com>
2023-11-02 16:47:18 -0300

    Allow Book to have null description

--- Models/Book.cs
+++ Models/Book.cs
@@ -2,8 +2,8 @@ namespace LibraryManagementSystemHtmx.Models;
 
 public class Book : BaseEntity
 {
-  public string Isbn { get; set; }
-  public string Title { get; set; }
-  public string Author { get; set; }
-  public string Description { get; set; }
+  public string Isbn { get; set; } = string.Empty;
+  public string Title { get; set; } = string.Empty;
+  public string Author { get; set; } = string.Empty;
+  public string? Description { get; set; } = null!;
 }
